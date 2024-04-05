0a467ebc518d7822b39006627b88c187aefce983
vip30service123 <thaingominh2652000@gmail.com>
2024-04-05 14:53:55 +0700

    add tokenized_labels_to_token_labels function.

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Tuple
 import json
 
 
@@ -9,5 +9,20 @@ def load_json(data_path: str) -> Dict:
 	return data	
 
 
-def tokenized_labels_to_token_labels() -> List[str]:
-	pass
+def tokenized_labels_to_token_labels(
+		offset_mapping: List[Tuple[int]], 
+		tokenized_labels: List[int], 
+		token_maps: List[int]
+	) -> List[int]:
+
+	token_labels = {}
+	for id, (start, end) in enumerate(offset_mapping):
+		if start == 0 and end == 0:
+			continue
+
+		if token_maps[start] == - 1:
+			start += 1
+		
+		token_labels[token_maps[start]] = tokenized_labels[id]
+	
+	return list(token_labels.values())
